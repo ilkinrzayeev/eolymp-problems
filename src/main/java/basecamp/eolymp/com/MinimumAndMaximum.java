94da8a279b11e91b3828f2c00361029e4d40d49b
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2024-11-04 00:42:37 +0400

    I found the minimum and maximum of two natural numbers.

---
+++ src/main/java/basecamp/eolymp/com/MinimumAndMaximum.java
@@ -0,0 +1,22 @@
+package basecamp.eolymp.com;
+
+import java.util.Scanner;
+
+public class MinimumAndMaximum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+
+        if (a > 1_000_000_000 || b > 1_000_000_000) {
+            System.out.println("Invalid Number");
+        } else {
+            if (a > b) {
+                System.out.println(a + " " + b);
+            } else {
+                System.out.println(b + " " + a);
+            }
+        }
+    }
+}
